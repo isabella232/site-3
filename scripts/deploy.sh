e394b1561801ead54b73be7587a64b0b7115796f
moody <moody.salem@gmail.com>
2019-07-06 16:26:35 -0400

    remove profiles from the deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Uses the AWS profile deploy-ethvault if available
-[[ $(aws configure --profile deploy-ethvault list) && $? -eq 0 ]] && \
-    AWS_DEPLOYMENT_PROFILE=deploy-ethvault || \
-    AWS_DEPLOYMENT_PROFILE=default
-
 BUCKET_NAME=app-ethvault-dev-stack-assetsbucket-1dmoq3zf0j7l3
 
-aws s3 --profile=${AWS_DEPLOYMENT_PROFILE} cp --recursive --exclude build/index.html build s3://$BUCKET_NAME
-aws s3 --profile=${AWS_DEPLOYMENT_PROFILE} cp --cache-control "max-age=0, s-max-age=0" build/index.html  s3://$BUCKET_NAME
+aws s3 cp --recursive --exclude build/index.html build s3://$BUCKET_NAME
+aws s3 cp --cache-control "max-age=0, s-max-age=0" build/index.html  s3://$BUCKET_NAME
