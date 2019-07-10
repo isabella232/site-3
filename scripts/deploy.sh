23f35d5cfdc985bb052fdce64772a5c8007c5a98
moody <moody.salem@gmail.com>
2019-07-10 09:51:02 -0400

    add a cache control header to the assets for 1 day

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 BUCKET_NAME=app-ethvault-dev-stack-assetsbucket-1dmoq3zf0j7l3
 
-aws s3 cp --recursive --exclude build/index.html build s3://$BUCKET_NAME
+aws s3 cp --cache-control "max-age=86400" --recursive --exclude build/index.html build s3://$BUCKET_NAME
 aws s3 cp --cache-control "max-age=0, s-max-age=0" build/index.html  s3://$BUCKET_NAME
