fe7f889e6d803017dcc9d829b2631afb1207a3a0
moody <moody.salem@gmail.com>
2019-07-11 19:48:33 -0400

    update to ethvault.xyz domain

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUCKET_NAME=app-ethvault-dev-stack-assetsbucket-1dmoq3zf0j7l3
+BUCKET_NAME=ethvault-xyz-assetsbucket-1xcfpdz5ziex9
 
 aws s3 cp --cache-control "max-age=86400" --recursive --exclude build/index.html build s3://$BUCKET_NAME
 aws s3 cp --cache-control "max-age=0, s-max-age=0" build/index.html  s3://$BUCKET_NAME
