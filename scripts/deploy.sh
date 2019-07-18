12070f8204d5b82c2cebd0617135189c2eecd186
moody <moody.salem@gmail.com>
2019-07-18 17:13:59 -0400

    cache slightly more aggressively (10 minutes on index.html)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@
 BUCKET_NAME=ethvault-xyz-assetsbucket-1xcfpdz5ziex9
 
 aws s3 cp --cache-control "max-age=86400" --recursive --exclude build/index.html build s3://$BUCKET_NAME
-aws s3 cp --cache-control "max-age=0, s-max-age=0" build/index.html  s3://$BUCKET_NAME
+aws s3 cp --cache-control "max-age=600" build/index.html  s3://$BUCKET_NAME
