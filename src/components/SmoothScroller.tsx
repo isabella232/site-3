0e6cc042d09cff9d17bcdbb5d358e28459a24e8b
Moody Salem <moody.salem@gmail.com>
2019-07-24 12:21:36 -0400

    use the ref instead of the synthetic events which are attached passively to the document

--- src/components/SmoothScroller.tsx
+++ src/components/SmoothScroller.tsx
@@ -1,64 +1,55 @@
-import React, { HTMLProps } from 'react';
+import React, { createRef, HTMLProps } from 'react';
 
 export interface SmoothScrollerProps extends HTMLProps<HTMLDivElement> {
 }
 
 /**
  * This class prevents scrolling past the top or bottom of a div on iOS
- * The rubber banding is speculated to cause the freezing in iOS
+ * The rubber banding behavior in iOS safari is speculated to cause the app to become unresponsive
  *
  * https://stackoverflow.com/questions/39692337/div-scrolling-freezes-sometimes-if-i-use-webkit-overflow-scrolling
  */
 export default class SmoothScroller extends React.Component<SmoothScrollerProps> {
   lastY: number = 0;
 
-  /**
-   * Record the Y offset of the touch
-   */
-  handleTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
-    this.lastY = event.touches[ 0 ].clientY;
+  ref = createRef<HTMLDivElement>();
 
-    if (this.props.onTouchStart) {
-      this.props.onTouchStart(event);
+  componentDidMount(): void {
+    if (this.ref.current) {
+      const div = this.ref.current;
+
+      this.ref.current.addEventListener('touchstart', event => {
+        this.lastY = event.touches[ 0 ].clientY;
+      }, { passive: false });
+
+      this.ref.current.addEventListener('touchmove', event => {
+        const top = event.touches[ 0 ].clientY;
+
+        // Determine scroll position and direction.
+        const scrollTop = div.scrollTop;
+        const direction = (this.lastY - top) < 0 ? 'up' : 'down';
+
+        if (event.cancelable) {
+          if (scrollTop === 0 && direction === 'up') {
+            // Prevent scrolling up when already at top as this introduces a freeze.
+            event.preventDefault();
+          } else if (
+            scrollTop >= (div.scrollHeight - div.clientHeight) &&
+            direction === 'down'
+          ) {
+            // Prevent scrolling down when already at bottom as this also introduces a freeze.
+            event.preventDefault();
+          }
+        }
+
+        this.lastY = top;
+      }, { passive: false });
     }
-  };
-
-  /**
-   * If the user is moving up or down past the top or bottom of the element respectively,
-   * prevent the touch from being handled
-   */
-  handleTouchMove = (event: React.TouchEvent<HTMLDivElement>) => {
-    const top = event.touches[ 0 ].clientY;
-
-    // Determine scroll position and direction.
-    const scrollTop = event.currentTarget.scrollTop;
-    const direction = (this.lastY - top) < 0 ? 'up' : 'down';
-
-    if (scrollTop === 0 && direction === 'up') {
-      // Prevent scrolling up when already at top as this introduces a freeze.
-      event.preventDefault();
-    } else if (
-      scrollTop >= (event.currentTarget.scrollHeight - event.currentTarget.clientHeight) &&
-      direction === 'down'
-    ) {
-      // Prevent scrolling down when already at bottom as this also introduces a freeze.
-      event.preventDefault();
-    }
-
-    this.lastY = top;
-
-    if (this.props.onTouchMove) {
-      this.props.onTouchMove(event);
-    }
-  };
+  }
 
   render() {
     return (
-      <div
-        {...this.props}
-        onTouchMove={this.handleTouchMove}
-        onTouchStart={this.handleTouchStart}
-      />
+      <div ref={this.ref} {...this.props}/>
     );
   }
 }
