6b3f12439a7fa9999a0c491ce0c8c1663962d59d
moody <moody.salem@gmail.com>
2019-07-31 22:09:53 -0400

    update the bucket name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUCKET_NAME=myethvault-com-assetsbucket-15y7aqgmui57t
+BUCKET_NAME=myethvault-site-assetsbucket-15vre6cxeaise
 
 aws s3 cp --cache-control "max-age=86400" --recursive --exclude build/index.html build s3://$BUCKET_NAME
 aws s3 cp --cache-control "max-age=600" build/index.html  s3://$BUCKET_NAME
